cfaf69e5019f525b691b2a68497d8bf897e2b027
TechWithAyaan <techwithayaanfaheem@gmail.com>
2025-07-12 13:08:28 -0700

    Pranking website

---
+++ app.js
@@ -0,0 +1,7 @@
+alert("I know what you're thinking — I can read your mind.");
+let ik = "You are thinking...";
+let enter = prompt("Enter a Number between 1-100");
+let num = "Number:"
+let emo = "🤣"
+let res = ik + num+enter+emo;
+document.writeln(res)
